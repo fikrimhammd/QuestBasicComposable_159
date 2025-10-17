cafe19a04dc0dcf3ffd729f12e96051f83db2389
fikrimhammd <fikri.kdi60@gmail.com>
2025-10-17 11:01:05 +0700

    Menambahkan tataletakColumn

--- app/src/main/java/com/example/layout/MainActivity.kt
+++ app/src/main/java/com/example/layout/MainActivity.kt
@@ -20,8 +20,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             LayoutTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                   TataletakColumn(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
